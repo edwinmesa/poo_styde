8078f64d457e5fc13b45ed8ac3c36b50ac2fe7b1
Edwin Mesa <edwinsar.mes@gmail.com>
2018-08-20 19:45:23 -0500

    Inicio de la Sesion 5 de Styde

---
+++ Sesion-5.php
@@ -0,0 +1,87 @@
+<?php
+
+function show($message) {
+    echo "<p>$message</p>";
+}
+
+abstract class Unit {
+
+    protected $hp = 40;
+    protected $alive = true;
+    protected $name;
+
+    public function setHp($points) {
+        $this->hp = $points;
+        show("{$this->name} ahora tiene {$this->hp} puntos de vida");
+    }
+
+    public function getHp() {
+        return $this->hp;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function move($direction) {
+        show("{$this->name} camina hacia $direction");
+    }
+
+    abstract public function attack(Unit $opponent);
+
+    public function takeDamage($damage) {
+        $this->setHp($this->hp - $damage);
+        if ($this->hp <= 0) {
+            $this->dier();
+        }
+    }
+
+    public function dier() {
+        show("{$this->name} muere");
+    }
+
+}
+
+class Soldier extends Unit {
+
+    protected $damage = 40;
+
+    public function attack(Unit $opponent) {
+        show("{$this->name} ataca con la espada a {$opponent->getName()}");
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage($damage) {
+        return parent::takeDamage($damage / 2);
+    }
+
+}
+
+class Archer extends Unit {
+
+    protected $damage = 20;
+
+    public function attack(Unit $opponent) {
+        show("{$this->name} dispara una flecha a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage($damage) {
+        if (rand(0, 1) == 1) {
+            return parent::takeDamage($damage);
+        }
+    }
+
+}
+
+$ramm = new Soldier('Ramm');
+$silence = new Archer('Silence');
+//$silence->move('el norte');
+$silence->attack($ramm);
+$silence->attack($ramm);
+$ramm->attack($silence);
