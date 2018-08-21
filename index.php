fb0cb09d6a6c2d8ee841272b8ecd771e1ab1ca48
Edwin Mesa <edwinsar.mes@gmail.com>
2018-08-20 22:02:00 -0500

    Comentarios de leccion 7

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-
+namespace Styde;
+require './src/helpers.php';
+/*
+ * Con esta funcion de PHP lo que hacemos es validar 
+ * que se carguen todas las clases de src, para evitar
+ * que el codigo se duplique, tanto 'requiere'
+ */
+spl_autoload_register(function ($className) {
+    if (strpos($className, 'Styde\\') === 0) {
+        $className = str_replace('Styde\\', '', $className);
+        if (file_exists("src/$className.php")) {
+            require "src/$className.php";
+        }
+    }
+//    exit($className);    
+});
 
 $armor = new BronzeArmor();
 $ramm = new Soldier('Ramm');
