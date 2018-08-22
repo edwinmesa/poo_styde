7121877f96c112c4c3e287ba6439bcc3629aba7c
Edwin Mesa <edwinsar.mes@gmail.com>
2018-08-21 20:25:04 -0500

    Fin de la leccion 6

--- public/index.php
+++ public/index.php
@@ -1,17 +1,12 @@
 <?php
 namespace Styde;
-require './src/helpers.php';
-/*
- * Con esta funcion de PHP lo que hacemos es validar 
- * que se carguen todas las clases de src, para evitar
- * que el codigo se duplique, tanto 'requiere'
- */
+require '../vendor/autoload.php';
 
 
-$armor = new BronzeArmor();
+$armor = new Armors\BronzeArmor();
 $ramm = new Soldier('Ramm');
 $silence = new Archer('Silence');
 $silence->attack($ramm);
-$ramm->setArmor(new CursedArmor);
+$ramm->setArmor(new Armors\CursedArmor);
 $silence->attack($ramm);
 $ramm->attack($silence);
