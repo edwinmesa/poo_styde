be4404540213da95c335b8840b1485e7e17c52c5
Edwin Mesa <edwinsar.mes@gmail.com>
2018-08-21 20:03:50 -0500

    cambiamos el codigo del autocarga

--- public/index.php
+++ public/index.php
@@ -6,15 +6,7 @@ require './src/helpers.php';
  * que se carguen todas las clases de src, para evitar
  * que el codigo se duplique, tanto 'requiere'
  */
-spl_autoload_register(function ($className) {
-    if (strpos($className, 'Styde\\') === 0) {
-        $className = str_replace('Styde\\', '', $className);
-        if (file_exists("src/$className.php")) {
-            require "src/$className.php";
-        }
-    }
-//    exit($className);    
-});
+
 
 $armor = new BronzeArmor();
 $ramm = new Soldier('Ramm');
