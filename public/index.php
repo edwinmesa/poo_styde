05aebc24c96806acfcbd52fef449195ebf5d15eb
Edwin Mesa <edwinsar.mes@gmail.com>
2018-08-23 20:14:42 -0500

    Se inicia la sesion 9

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 namespace Styde;
 require '../vendor/autoload.php';
 
-$ramm = new Soldier('Ramm',new Weapons\BasicSword);
+$ramm = new Unit('Ramm',new Weapons\BasicSword);
 $ramm->setArmor(new Armors\BronzeArmor());
-$silence = new Archer('Silence', new Weapons\CrossBow());
+$silence = new Unit('Silence', new Weapons\CrossBow());
 $silence->attack($ramm);
 $silence->attack($ramm);
 $ramm->attack($silence);
